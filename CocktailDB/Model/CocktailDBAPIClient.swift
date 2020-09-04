f68ff0641016ee5ae99e20c7ab37afb892f7d876
akulaiev <kulaeva.a@ukr.net>
2020-09-04 16:04:28 +0300

    Initial commit

---
+++ CocktailDB/Model/CocktailDBAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  CocktailDBAPIClient.swift
+//  CocktailDB
+//
+//  Created by Anna Kulaieva on 04.09.2020.
+//  Copyright © 2020 Anna Kulaieva. All rights reserved.
+//
+
+import Foundation
+
+class CocktailDBAPIClient {
+    
+}
---
+++ CocktailDB/View Controllers/DrinksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DrinksViewController.swift
+//  CocktailDB
+//
+//  Created by Anna Kulaieva on 04.09.2020.
+//  Copyright © 2020 Anna Kulaieva. All rights reserved.
+//
+
+import UIKit
+
+class DrinksViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+extension DrinksViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
